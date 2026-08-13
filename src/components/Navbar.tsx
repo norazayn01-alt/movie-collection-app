15a9c02a5f07c6e1b6cef458a1a910df62111b3c
norazayn01-alt <norazayn01@gmail.com>
2026-08-13 12:59:03 +0500

    add new something xaxaxax

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,11 @@
 import { Group, Button } from "@mantine/core";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { TextInput } from "@mantine/core";
 
 export function Navbar() {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
   return (
     <Group
       justify="space-between"
@@ -16,7 +21,16 @@ export function Navbar() {
       <h3 style={{ color: "white" }}>
         <span style={{ color: "#E50914" }}>MoV</span>iE
       </h3>
-
+      <TextInput
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search movies..."
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            navigate(`/search?query=${query}`);
+          }
+        }}
+      />
       <Group>
         <Button
           styles={{
